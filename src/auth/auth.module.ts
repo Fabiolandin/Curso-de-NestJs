9792c8d94f0b92751505af2c3d7fc971d6cc885f
Fabio Landin <flandin1990@gmail.com>
2026-04-08 09:27:18 -0300

    feat:adicionando authenticate nas rotas

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
 
 @Module({
-  imports: [UserModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_KEY || '',
-      signOptions: { expiresIn: '1h' },
-    }),
+  imports: [forwardRef(() => UserModule),
+  JwtModule.register({
+    global: true,
+    secret: process.env.SECRET_KEY || '',
+    signOptions: { expiresIn: '1h' },
+  }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AuthModule { }
