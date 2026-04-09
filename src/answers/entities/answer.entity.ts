5f7cda0666d82bda4a3610fbb6c886dee66e045e
Fabio Landin <flandin1990@gmail.com>
2026-04-09 09:54:48 -0300

    add:entityes

--- src/answers/entities/answer.entity.ts
+++ src/answers/entities/answer.entity.ts
@@ -1,1 +1,14 @@
-export class Answer {}
+import { User } from "src/user/entities/user.entity";
+import { Question } from "src/questions/entities/question.entity";
+import { Answers } from "@prisma/client";
+
+export class Answer implements Answers {
+    id: number;
+    body: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: number;
+    questionId: number;
+    user: User;
+    question: Question;
+}
