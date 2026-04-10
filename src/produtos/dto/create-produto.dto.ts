44b19e9a2359a31993753cd1f3d2f3c09226a033
Fabio Landin <flandin1990@gmail.com>
2026-04-10 13:40:27 -0300

    feat:validação de produtos

--- src/produtos/dto/create-produto.dto.ts
+++ src/produtos/dto/create-produto.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
+
 export class CreateProdutoDto {
+    @IsNotEmpty()
+    @IsString()
     nome: string
+
+    @IsNotEmpty()
+    @IsNumber()
     preco: number
+
+    @IsNotEmpty()
+    @IsString()
     descricao: string
 }
--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,14 +1,16 @@
-import { IsAlpha, IsEmail, IsNotEmpty, IsString, Min } from "class-validator";
+import { IsAlpha, IsEmail, IsNotEmpty, IsString, Length, Min } from "class-validator";
 
 export class CreateUserDto {
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @Length(3)
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @Length(4, 20)
     @IsAlpha()
     @IsNotEmpty()
     password: string;
